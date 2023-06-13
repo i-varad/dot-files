a37df70a41e085e080492ce3e43afa04385b90b1
i-varad <ivaradsalgare@gmail.com>
2023-06-13 15:24:54 +0530

    Update

---
+++ .config/Code - OSS/User/History/-60420cde/1wX5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+# polybar example --config=~/.config/polybar/config.ini &
+
+polybar example &
+polybar example_ext &
+
+echo "Polybar launched..."
